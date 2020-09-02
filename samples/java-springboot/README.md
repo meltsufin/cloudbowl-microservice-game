7d83aaa327ab46e090db437adb1e61d9b2a398ce
Mike Eltsufin <meltsufin@google.com>
2020-09-02 16:24:09 -0400

    Initial version of the bot

--- samples/java-springboot/README.md
+++ samples/java-springboot/README.md
@@ -9,3 +9,16 @@ Run Locally:
 ```
 
 [![Run on Google Cloud](https://deploy.cloud.run/button.svg)](https://deploy.cloud.run)
+
+
+1. Build and push the image to Container Registry.
+    ```
+    export PROJECT_ID=your-project-id
+    export SAMPLE=java-spring 
+    mvn compile -Dimage=gcr.io/${PROJECT_ID}/$SAMPLE jib:build
+    ```
+
+1. Deploy to Cloud Run.
+    ```
+    gcloud run deploy $SAMPLE --image gcr.io/$PROJECT_ID/$SAMPLE --platform managed --region us-central1 --memory=512Mi --allow-unauthenticated
+    ```
--- samples/java-springboot/src/main/java/hello/Application.java
+++ samples/java-springboot/src/main/java/hello/Application.java
@@ -12,6 +12,7 @@ import java.util.Random;
 @SpringBootApplication
 @RestController
 public class Application {
+	private final static int RANGE = 3;
 
   static class Self {
     public String href;
@@ -50,12 +51,39 @@ public class Application {
 
   @GetMapping("/")
   public String index() {
-    return "Let the battle begin!";
+    return "Let the battle begin!!!";
   }
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
     System.out.println(arenaUpdate);
+
+    PlayerState self = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    for (String href : arenaUpdate.arena.state.keySet()) {
+      if (!href.equals(arenaUpdate._links.self.href)) {
+        PlayerState state = arenaUpdate.arena.state.get(href);
+        int dx = state.x - self.x;
+        int dy = state.y - self.y;
+        if (self.direction.equals("N")) {
+          if (dy < 0 && dy >= -RANGE) {
+            return "F";
+          }
+        } else if (self.direction.equals("S")) {
+          if (dy > 0 && dy <= RANGE) {
+            return "F";
+          }
+        } else if (self.direction.equals("W")) {
+          if (dx < 0 && dx >= -RANGE) {
+            return "F";
+          }
+        } else if (self.direction.equals("E")) {
+          if (dx > 0 && dx <= RANGE) {
+            return "F";
+          }
+        }
+      }
+    }
+
     String[] commands = new String[]{"F", "R", "L", "T"};
     int i = new Random().nextInt(4);
     return commands[i];
