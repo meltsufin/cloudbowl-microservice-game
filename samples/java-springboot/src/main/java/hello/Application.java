e6d11e593008c26d476a5415adbf6ace3171ad14
Mike Eltsufin <meltsufin@google.com>
2020-09-02 23:54:06 -0400

    some fixes and strategies

--- samples/java-springboot/src/main/java/hello/Application.java
+++ samples/java-springboot/src/main/java/hello/Application.java
@@ -12,7 +12,7 @@ import java.util.Random;
 @SpringBootApplication
 @RestController
 public class Application {
-	private final static int RANGE = 3;
+  static int RANGE = 3;
 
   static class Self {
     public String href;
@@ -56,38 +56,125 @@ public class Application {
 
   @PostMapping("/**")
   public String index(@RequestBody ArenaUpdate arenaUpdate) {
-    System.out.println(arenaUpdate);
+    String result = null;
 
+    if (iAmATarget(arenaUpdate)) {
+      result = getRandomAction(new String[]{"F", "R"});
+      System.out.println("I am in range. Random move: " + result);
+    } else if (isTargetInRange(arenaUpdate)) {
+      result = "T";
+      System.out.println("Target is in range. Throwing!");
+    } else {
+      result = getMoveTowardsCenter(arenaUpdate);
+      System.out.println("Target is not range. Move towards center: " + result);
+
+    }
+
+    return result;
+  }
+
+  private boolean iAmATarget(ArenaUpdate arenaUpdate) {
     PlayerState self = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
     for (String href : arenaUpdate.arena.state.keySet()) {
       if (!href.equals(arenaUpdate._links.self.href)) {
         PlayerState state = arenaUpdate.arena.state.get(href);
         int dx = state.x - self.x;
         int dy = state.y - self.y;
-        if (self.direction.equals("N")) {
-          if (dy < 0 && dy >= -RANGE) {
-            return "F";
+        // System.out.printf("dx = %s dy = %s; facing = %s\n", dx, dy, self.direction);
+        if (state.direction.equals("S")) {
+          if (dx == 0 && dy < 0 && dy >= -RANGE) {
+            return true;
           }
-        } else if (self.direction.equals("S")) {
-          if (dy > 0 && dy <= RANGE) {
-            return "F";
+        } else if (state.direction.equals("N")) {
+          if (dx == 0 && dy > 0 && dy <= RANGE) {
+            return true;
           }
-        } else if (self.direction.equals("W")) {
-          if (dx < 0 && dx >= -RANGE) {
-            return "F";
+        } else if (state.direction.equals("E")) {
+          if (dy == 0 && dx < 0 && dx >= -RANGE) {
+            return true;
           }
-        } else if (self.direction.equals("E")) {
-          if (dx > 0 && dx <= RANGE) {
-            return "F";
+        } else if (state.direction.equals("W")) {
+          if (dy == 0 && dx > 0 && dx <= RANGE) {
+            return true;
           }
         }
       }
     }
-
-    String[] commands = new String[]{"F", "R", "L", "T"};
-    int i = new Random().nextInt(4);
-    return commands[i];
+    return false;
   }
 
+  private boolean isTargetInRange(ArenaUpdate arenaUpdate) {
+    PlayerState self = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    for (String href : arenaUpdate.arena.state.keySet()) {
+      if (!href.equals(arenaUpdate._links.self.href)) {
+        PlayerState state = arenaUpdate.arena.state.get(href);
+        int dx = state.x - self.x;
+        int dy = state.y - self.y;
+        // System.out.printf("dx = %s dy = %s; facing = %s\n", dx, dy, self.direction);
+        if (self.direction.equals("N")) {
+          if (dx == 0 && dy < 0 && dy >= -RANGE) {
+            return true;
+          }
+        } else if (self.direction.equals("S")) {
+          if (dx == 0 && dy > 0 && dy <= RANGE) {
+            return true;
+          }
+        } else if (self.direction.equals("W")) {
+          if (dy == 0 && dx < 0 && dx >= -RANGE) {
+            return true;
+          }
+        } else if (self.direction.equals("E")) {
+          if (dy == 0 && dx > 0 && dx <= RANGE) {
+            return true;
+          }
+        }
+      }
+    }
+    return false;
+  }
+
+  private String getRandomAction(String[] commands) {
+    String result;
+    int i = new Random().nextInt(commands.length);
+    result = commands[i];
+    return result;
+  }
+
+  private String getMoveTowardsCenter(ArenaUpdate arenaUpdate) {
+    PlayerState self = arenaUpdate.arena.state.get(arenaUpdate._links.self.href);
+    int w = arenaUpdate.arena.dims.get(0);
+    int h = arenaUpdate.arena.dims.get(1);
+    int dx = self.x - w / 2;
+    int dy = self.y - h / 2;
+    if (Math.abs(dx) > Math.abs(dy)) {
+      if (dx < 0) {
+        if (self.direction.equals("E")) {
+          return "F";
+        } else {
+          return "R";
+        }
+      } else {
+        if (self.direction.equals("W")) {
+          return "F";
+        } else {
+          return "R";
+        }
+      }
+    } else {
+      if (dy < 0) {
+        if (self.direction.equals("S")) {
+          return "F";
+        } else {
+          return "R";
+        }
+      } else {
+        if (self.direction.equals("N")) {
+          return "F";
+        } else {
+          return "R";
+        }
+      }
+    }
+  }
 }
 
---
+++ samples/java-springboot/src/test/java/hello/ApplicationTests.java
@@ -0,0 +1,137 @@
+package hello;
+
+import hello.Application;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.http.*;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT, classes = {Application.class})
+public class ApplicationTests {
+
+  @Autowired
+  private TestRestTemplate template;
+
+  @LocalServerPort
+  private int port;
+
+  @Test
+  public void testTargetInRange() throws URISyntaxException {
+    String url = String.format("http://localhost:%s/", this.port);
+    String body = "{\n" +
+            "  \"_links\": {\n" +
+            "    \"self\": {\n" +
+            "      \"href\": \"https://foo.com\"\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"arena\": {\n" +
+            "    \"dims\": [4,3],\n" +
+            "    \"state\": {\n" +
+            "      \"https://foo.com\": {\n" +
+            "        \"x\": 2,\n" +
+            "        \"y\": 2,\n" +
+            "        \"direction\": \"N\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      },\n" +
+            "      \"1\": {\n" +
+            "        \"x\": 2,\n" +
+            "        \"y\": 0,\n" +
+            "        \"direction\": \"N\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+    RequestEntity request = RequestEntity.post(new URI(url)).contentType(MediaType.APPLICATION_JSON).body(body);
+    ResponseEntity<String> response = template.exchange(request, String.class);
+
+    assertThat(response.getBody()).isEqualTo("T");
+  }
+
+  @Test
+  public void testIamInRange() throws URISyntaxException {
+    String url = String.format("http://localhost:%s/", this.port);
+    String body = "{\n" +
+            "  \"_links\": {\n" +
+            "    \"self\": {\n" +
+            "      \"href\": \"https://foo.com\"\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"arena\": {\n" +
+            "    \"dims\": [4,3],\n" +
+            "    \"state\": {\n" +
+            "      \"https://foo.com\": {\n" +
+            "        \"x\": 4,\n" +
+            "        \"y\": 0,\n" +
+            "        \"direction\": \"N\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      },\n" +
+            "      \"1\": {\n" +
+            "        \"x\": 2,\n" +
+            "        \"y\": 0,\n" +
+            "        \"direction\": \"E\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+    RequestEntity request = RequestEntity.post(new URI(url)).contentType(MediaType.APPLICATION_JSON).body(body);
+    ResponseEntity<String> response = template.exchange(request, String.class);
+
+    assertThat(response.getBody()).isNotEqualTo("T");
+  }
+
+  @Test
+  public void testMoveTowardsCenter() throws URISyntaxException {
+    String url = String.format("http://localhost:%s/", this.port);
+    String body = "{\n" +
+            "  \"_links\": {\n" +
+            "    \"self\": {\n" +
+            "      \"href\": \"https://foo.com\"\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"arena\": {\n" +
+            "    \"dims\": [12,6],\n" +
+            "    \"state\": {\n" +
+            "      \"https://foo.com\": {\n" +
+            "        \"x\": 0,\n" +
+            "        \"y\": 0,\n" +
+            "        \"direction\": \"E\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      },\n" +
+            "      \"1\": {\n" +
+            "        \"x\": 2,\n" +
+            "        \"y\": 4,\n" +
+            "        \"direction\": \"N\",\n" +
+            "        \"wasHit\": false,\n" +
+            "        \"score\": 0\n" +
+            "      }\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
+
+    RequestEntity request = RequestEntity.post(new URI(url)).contentType(MediaType.APPLICATION_JSON).body(body);
+    ResponseEntity<String> response = template.exchange(request, String.class);
+
+    assertThat(response.getBody()).isEqualTo("F");
+  }
+}
